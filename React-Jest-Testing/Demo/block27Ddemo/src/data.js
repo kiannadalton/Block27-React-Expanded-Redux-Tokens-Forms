7caf5d959617ee2fa8989e3270bae2bb3eea5b3f
kiannadalton <kiannadalton@aol.com>
2025-10-15 14:06:36 -0400

    Added baseline code for the Block 27D Demo.

---
+++ React-Jest-Testing/Demo/block27Ddemo/src/data.js
@@ -0,0 +1,17 @@
+export const taskList = [
+  {
+    task: "Start Block 27A Workshop",
+    id: 1,
+    completed: false,
+  },
+  {
+    task: "Start Block 27B Workshop",
+    id: 2,
+    completed: false,
+  },
+  {
+    task: "Start Block 27C Workshop",
+    id: 3,
+    completed: false,
+  },
+];
